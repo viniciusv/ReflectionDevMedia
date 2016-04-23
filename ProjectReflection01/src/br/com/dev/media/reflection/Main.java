030fd433cd1f24abcac7bb9c86d518244984022a
Vinicius Viana <vinicius.viana@al.infnet.edu.br>
2016-04-23 11:37:22 -0300

    Primeira Aula de reflection pela devmedia

--- ProjectReflection01/src/br/com/dev/media/reflection/Main.java
+++ ProjectReflection01/src/br/com/dev/media/reflection/Main.java
@@ -1,10 +1,34 @@
 package br.com.dev.media.reflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Class classe = Pessoa.class;		
+		
+		System.out.println("Imprimindo os nomes dos atributos da classe:" + classe.getSimpleName());
+		System.out.println("IMprimindo os atributos");
+		for (Field atributo : classe.getDeclaredFields()) {
+			System.out.println("Nome do atributo:" + atributo.getName());
+			System.out.println("com os Modificadores:" + Modifier.toString(atributo.getModifiers()));
+		}
+		
+		System.out.println("IMprimindo os metodos");
+		for (Method metodo : classe.getDeclaredMethods()) {
+			System.out.println("Nome dos metodos:" + metodo.getName());
+			System.out.println("com os Modificadores:" + Modifier.toString(metodo.getModifiers()));			
+		}
+		
+		//class -  representa a classe
+		//field - representa os atributos
+		//method - representa os metados da classe
+		//constructor - representa os construtores
+		//modifier - modificadores de acessos
+		//package - nossos pacotes 
+		//Proxy - Um interceptador.. entre a chama de um metodo e a execucao dele.
 	}
 
 }
--- ProjectReflection01/src/br/com/dev/media/reflection/Pessoa.java
+++ ProjectReflection01/src/br/com/dev/media/reflection/Pessoa.java
@@ -2,8 +2,46 @@ package br.com.dev.media.reflection;
 
 public class Pessoa {
 
+	protected static String nome;
+	protected Integer idade;
+	private String endereco;
+	public static Double altura;
+	
+		
+	protected static String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public final Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	private String getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public Double getAltura() {
+		return altura;
+	}
+	
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+	
 	public Pessoa() {
-		// TODO Auto-generated constructor stub
+	
 	}
 
 }
